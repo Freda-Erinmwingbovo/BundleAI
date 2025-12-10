860a02fee45ae878b1ea368ff91fa89bfb79b1c4
Freda Erinmwingbovo <engrfreda@gmail.com>
2025-12-10 09:40:24 +0100

    Create 3_Contact.py

---
+++ pages/3_Contact.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("3. Contact")
+
+st.markdown("""
+**BundleAI**  
+Built by **Freda Erinmwingbovo**  
+Data Scientist | Retail Analytics | Python
+
+📧 fredaerins@gmail.com  
+🔗 https://www.linkedin.com/in/freda-erinmwingbovo 
+🐙 https://github.com/Freda-Erinmwingbovo/BundleAI
+
+Have questions or want a custom version? Get in touch!
+""")
